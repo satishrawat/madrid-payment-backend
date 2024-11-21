88597e985d2ee89c7bf0272d7a16da48a5e1e5bd
satishrawat <39967722+satishrawat@users.noreply.github.com>
2024-11-21 16:02:49 +0100

    Update PaymentRepository.java

--- src/main/java/com.madridpayment/repository/PaymentRepository.java
+++ src/main/java/com.madridpayment/repository/PaymentRepository.java
@@ -3,7 +3,8 @@ package com.madridpayment.repository;
 import com.madridpayment.model.Payment;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface PaymentRepository extends JpaRepository<Payment, Long> {
-    Payment findByReference(String reference);
-}
+import java.util.Optional;
 
+public interface PaymentRepository extends JpaRepository<Payment, Long> {
+    Optional<Payment> findByReference(String reference);
+}
